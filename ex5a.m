0b9b23680ec369a5343f02f356fce43d20fbc4b0
radulescuvladd <44034208+radulescuvladd@users.noreply.github.com>
2018-11-07 01:08:27 +0200

    Delete ex5a.m

--- ex5a.m
+++
@@ -1,9 +0,0 @@
-fs = 50;  %frecventa lui sinus
-fc = 20;  %frecventa lui cosinus
-t = 0:0.001:0.2; 
-a = 2*sin(2*pi*fs*t);
-b = 2*cos(2*pi*fc*t); % amplitudinea este egala cu cea a semnalului sinusoidal
-plot(t,a),grid;
-hold on;
-plot(t,b,'r'),grid;
-hold off;
